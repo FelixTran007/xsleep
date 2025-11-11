7ea9a4722cf8817a6a2c64efa03c65fd20b736ef
FelixTran007 <felixtran007@gmail.com>
2025-11-11 19:33:47 +0700

    Update header_banner.php

--- powerxsleep/app/Views/frontend/header_banner.php
+++ powerxsleep/app/Views/frontend/header_banner.php
@@ -2,72 +2,30 @@
 <div class="hero-slider-area position-relative">
     <div class="swiper hero-slider-container">
         <div class="swiper-wrapper">
-            <div class="swiper-slide hero-slide-item" data-bg-img="/assets/images/slider/slider1-bg1.png">
-                <div class="container h-100">
-                    <div class="row align-items-center position-relative h-100">
-                        <div class="col-lg-6 col-md-7 col-sm-12 col-12">
-                            <div class="hero-slide-content">
-                                <h3 class="hero-slide-sub-title">A watch is a portable</h3>
-                                <h1 class="hero-slide-title">Wooden Store</h1>
-                                <p class="hero-slide-desc">Experience the decibels like your ears deserve to. Safe for the ears, <br class="d-none d-xl-block">very for the heart.</p>
-                                <div class="btn-wrp d-flex align-items-center justify-content-center justify-content-md-start">
-                                    <a class="btn btn-secondary" href="shop.html"> Explore More <i class="icon icofont-cart-alt"></i></a>
-                                    <div class="btn-play-wrp ms-4 ms-lg-8"><a class="btn-play ht-popup-video" data-fancybox data-type="iframe" href="https://www.youtube.com/watch?v=_9VUPq3SxOc"> <i class="icon icofont-play"></i></a> View Video</div>
+            <?php foreach ($banners as $banner): ?>
+                <div class="swiper-slide hero-slide-item" data-bg-img="/assets/images/slider/slider1-bg1.png">
+                    <div class="container h-100">
+                        <div class="row align-items-center position-relative h-100">
+                            <div class="col-lg-6 col-md-7 col-sm-12 col-12">
+                                <div class="hero-slide-content">
+                                    <h3 class="hero-slide-sub-title"><?= $banner['color_text'] ?></h3>
+                                    <h1 class="hero-slide-title"<?= $banner['big_text'] ?></h1>
+                                    <div><?= $banner['description_text'] ?></div>
+                                    <div class="btn-wrp d-flex align-items-center justify-content-center justify-content-md-start">
+                                        <a class="btn btn-secondary" href="<?= $banner['xem_them_link'] ?>"> <?= $banner['xem_them_text'] ?> <i class="icon icofont-cart-alt"></i></a>
+                                        <div class="btn-play-wrp ms-4 ms-lg-8"><a class="btn-play ht-popup-video" data-fancybox data-type="iframe" href="<?= $banner['video_link'] ?>"> <i class="icon icofont-play"></i></a> <?= $banner['video_text'] ?></div>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="col-lg-6 col-md-5 col-sm-12 col-12">
-                            <div class="hero-slide-thumb">
-                                <img src="assets/images/slider/slider1-2.png" width="570" height="474" alt="Image">
+                            <div class="col-lg-6 col-md-5 col-sm-12 col-12">
+                                <div class="hero-slide-thumb">
+                                    <img src="<?= base_url().$banner['image_path'] ?>" width="570" height="474" alt="Image">
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="swiper-slide hero-slide-item" data-bg-img="/assets/images/slider/slider1-bg1.png">
-                <div class="container h-100">
-                    <div class="row align-items-center position-relative h-100">
-                        <div class="col-lg-6 col-md-7 col-sm-12 col-12">
-                            <div class="hero-slide-content">
-                                <h3 class="hero-slide-sub-title">A watch is a portable</h3>
-                                <h1 class="hero-slide-title">Smart Furniture</h1>
-                                <p class="hero-slide-desc">Experience the decibels like your ears deserve to. Safe for <br class="d-none d-xl-block">the ears, very for the heart. A treat to your ears.</p>
-                                <div class="btn-wrp d-flex align-items-center justify-content-center justify-content-md-start">
-                                    <a class="btn btn-secondary" href="shop.html"> Explore More <i class="icon icofont-cart-alt"></i></a>
-                                    <div class="btn-play-wrp ms-4 ms-lg-8"><a class="btn-play ht-popup-video" data-fancybox data-type="iframe" href="https://www.youtube.com/watch?v=_9VUPq3SxOc"> <i class="icon icofont-play"></i></a> View Video</div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-lg-6 col-md-5 col-sm-12 col-12">
-                            <div class="hero-slide-thumb">
-                                <img src="assets/images/slider/slider1-1.png" width="570" height="570" alt="Image">
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="swiper-slide hero-slide-item" data-bg-img="/assets/images/slider/slider1-bg1.png">
-                <div class="container h-100">
-                    <div class="row align-items-center position-relative h-100">
-                        <div class="col-lg-6 col-md-7 col-sm-12 col-12">
-                            <div class="hero-slide-content">
-                                <h3 class="hero-slide-sub-title">A watch is a portable</h3>
-                                <h1 class="hero-slide-title">Smart Furniture Minimal Store</h1>
-                                <p class="hero-slide-desc">Experience the decibels like your ears deserve to. Safe for <br class="d-none d-xl-block">the ears, very for the heart. A treat to your ears.</p>
-                                <div class="btn-wrp d-flex align-items-center justify-content-center justify-content-md-start">
-                                    <a class="btn btn-secondary" href="shop.html"> Explore More <i class="icon icofont-cart-alt"></i></a>
-                                    <div class="btn-play-wrp ms-4 ms-lg-8"><a class="btn-play ht-popup-video" data-fancybox data-type="iframe" href="https://www.youtube.com/watch?v=_9VUPq3SxOc"> <i class="icon icofont-play"></i></a> View Video</div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-lg-6 col-md-5 col-sm-12 col-12">
-                            <div class="hero-slide-thumb">
-                                <img src="assets/images/slider/slider1-3.png" width="570" height="375" alt="Image">
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <?php endforeach; ?>            
         </div>
         <!--== Add Pagination ==-->
         <div class="hero-slide-one-pagination"></div>
